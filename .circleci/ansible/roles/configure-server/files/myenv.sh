7cfbf2638a6ea5185bcc19cd3932ae1ee9cd9b38
Mai Dieu <vuthimaidieu378460@gmail.com>
2023-08-26 14:06:53 +0700

    add configure server

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,10 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_DATABASE=postgres
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=udacity-project3.chwwkzlrnh0h.us-east-1.rds.amazonaws.com
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=password
+export TYPEORM_PORT=5432
+export AWS_DEFAULT_REGION=us-east-1
